ec0d782587b8249846d9d79c26fc4ad1dc896b2e
Mshkp <yeudielgonzalez42@gmail.com>
2026-03-29 19:14:22 -0600

    fix(backend): integra Resend como proveedor SMTP para correos oficiales

--- config/settings.py
+++ config/settings.py
@@ -197,10 +197,21 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-# OJO: Aquí agarra las contraseñas de las variables de entorno de Render
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'yeudiel.gonzalez@talent-hub.me') 
+# Configuración de Correo Electrónico (Vía Resend)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.resend.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+# Configuración de Correo Electrónico (Vía Resend)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.resend.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'resend') 
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '') 
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = 'TalentHub México <hola@talent-hub.me>'
 
 
 MEDIA_URL = '/media/'
