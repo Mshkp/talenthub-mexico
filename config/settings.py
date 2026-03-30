bd07f5e594c38a995a2d12a287bdfaab48d80c8e
Mshkp <yeudielgonzalez42@gmail.com>
2026-03-29 19:31:30 -0600

    fix(backend): cambia SMTP por API de Resend usando django-anymail para bypassear firewall de render

--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'anymail',
 ]
 
 MIDDLEWARE = [
@@ -186,32 +187,17 @@ SIMPLE_JWT = {
 }
 
 
-# Configuración para subir archivos (Media)
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Configuración de Correo Electrónico (Vía API HTTPS de Resend)
+EMAIL_BACKEND = "anymail.backends.resend.EmailBackend"
 
+ANYMAIL = {
+    # Jalamos tu API Key de las variables de entorno de Render
+    "RESEND_API_KEY": os.environ.get('EMAIL_HOST_PASSWORD', '')
+}
 
-# Configuración de Correo Electrónico
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-# Configuración de Correo Electrónico (Vía Resend)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.resend.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-# Configuración de Correo Electrónico (Vía Resend)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.resend.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'resend') 
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '') 
 DEFAULT_FROM_EMAIL = 'TalentHub México <hola@talent-hub.me>'
+
+
 
 
 MEDIA_URL = '/media/'
